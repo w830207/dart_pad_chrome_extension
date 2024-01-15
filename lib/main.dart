24697c32b88a495b4941aa67d51ded463658f1c8
w830207 <44136372+w830207@users.noreply.github.com>
2024-01-16 00:08:48 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:dart_pad_widget/dart_pad_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DartPad(
+      key: const Key('example1'),
+      code: r"""void main() {
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+}""",
+    );
+  }
+}
